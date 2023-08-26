28f50be2bd5f8a011d82c1f2e16336b6bf8c87e9
HemanthGaddey <gaddeyhemanth@gmail.com>
2023-08-27 00:45:03 +0530

    added results page check to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:prac/pages/intro_screen.dart';
 import 'package:prac/pages/home_screen.dart';
 import 'package:prac/pages/login_screen.dart';
+import 'package:prac/pages/results_screen.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -10,7 +11,11 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
+  static var l = {
+    'tomato yellow virus': 92,
+    'tomato late blight': 7,
+    'potato early blight': 1
+  };
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
