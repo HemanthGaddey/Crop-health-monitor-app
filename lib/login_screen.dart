35dc25df56ca718020069bbdadc960907053cb53
HemanthGaddey <gaddeyhemanth@gmail.com>
2023-09-07 21:36:03 +0530

    fixed image issue and also improved alignment of login page

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' // new
-    hide EmailAuthProvider, PhoneAuthProvider;    // new
-import 'package:provider/provider.dart';          // new
+    hide
+        EmailAuthProvider,
+        PhoneAuthProvider; // new
+import 'package:provider/provider.dart'; // new
 
-import 'app_state.dart';                          // new
+import 'app_state.dart'; // new
 import 'pages/authentication.dart';
 
 class Loginp extends StatefulWidget {
@@ -23,41 +25,39 @@ class _LoginState extends State<Loginp> {
         width: double.infinity,
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('Images/Background_.jpg'),
-            fit: BoxFit.cover
-          ),
+              image: AssetImage('assets/Images/Background_.jpg'),
+              fit: BoxFit.cover),
         ),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              
               Padding(
                 padding: const EdgeInsets.all(50.0),
                 child: Container(
-                 height: 200, 
-                 width: double.infinity,
-                 color: const Color.fromARGB(199, 237, 241, 214),
-                 child: Column(
-                  children: [
-                    // Padding(
-                    //   padding: const EdgeInsets.all(15.0),
-                    //   child: Text('SignIn to experience the effect of AI on monitoring',
-                    //   style: GoogleFonts.raleway(
-                    //     fontWeight: FontWeight.w500,
-                    //     fontSize: 25,
-                    //   ),
-                    //   ),
-                    // ),
-                    // ElevatedButton(
-                    //   onPressed: dummy,
-                    //   child: Text('Sign In with Google',
-                    //   style: GoogleFonts.raleway(
-                    //     fontWeight: FontWeight.w500,
-                    //     fontSize: 25,
-                    //   ),
-                    //   )
-                    //   ),
+                  height: 200,
+                  width: double.infinity,
+                  color: const Color.fromARGB(199, 237, 241, 214),
+                  child: Column(
+                    children: [
+                      // Padding(
+                      //   padding: const EdgeInsets.all(15.0),
+                      //   child: Text('SignIn to experience the effect of AI on monitoring',
+                      //   style: GoogleFonts.raleway(
+                      //     fontWeight: FontWeight.w500,
+                      //     fontSize: 25,
+                      //   ),
+                      //   ),
+                      // ),
+                      // ElevatedButton(
+                      //   onPressed: dummy,
+                      //   child: Text('Sign In with Google',
+                      //   style: GoogleFonts.raleway(
+                      //     fontWeight: FontWeight.w500,
+                      //     fontSize: 25,
+                      //   ),
+                      //   )
+                      //   ),
                       Consumer<ApplicationState>(
                         builder: (context, appState, _) => AuthFunc(
                             loggedIn: appState.loggedIn,
@@ -65,8 +65,8 @@ class _LoginState extends State<Loginp> {
                               FirebaseAuth.instance.signOut();
                             }),
                       ),
-                  ],
-                 ),
+                    ],
+                  ),
                 ),
               ),
             ],
--- lib/pages/authentication.dart
+++ lib/pages/authentication.dart
@@ -23,36 +23,38 @@ class AuthFunc extends StatelessWidget {
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.only(left: 24, bottom: 8),
-          child: ElevatedButton(
-                      onPressed: () {
-                        !loggedIn ? context.push('/sign-in') : signOut();
-                      },
-                      child: Text('Sign In',
-                      style: GoogleFonts.raleway(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 25,
-                      ),
-                      )
-                      )
-          // child: StyledButton(
-          //     onPressed: () {
-          //       !loggedIn ? context.push('/sign-in') : signOut();
-          //     },
-          //     child: !loggedIn ? const Text('RSVP') : const Text('Logout')),
-        ),
+            padding:
+                const EdgeInsets.only(left: 24, right: 24, top: 24, bottom: 8),
+            child: ElevatedButton(
+                onPressed: () {
+                  !loggedIn ? context.push('/sign-in') : signOut();
+                },
+                child: Text(
+                  'Sign In',
+                  style: GoogleFonts.raleway(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 25,
+                  ),
+                ))
+            // child: StyledButton(
+            //     onPressed: () {
+            //       !loggedIn ? context.push('/sign-in') : signOut();
+            //     },
+            //     child: !loggedIn ? const Text('RSVP') : const Text('Logout')),
+            ),
         Visibility(
-            visible: loggedIn,
-            // child: const HomePage(),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 24, bottom: 8),
-              child: StyledButton(
-                  onPressed: () {
-                    context.push('/home');
-                  },
-                  child: const Text('Go to Home')),
-            )
-            )
+          visible: !loggedIn,
+          // child: const HomePage(),
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 30, right: 24, top: 24, bottom: 8),
+            child: StyledButton(
+                onPressed: () {
+                  context.push('/home');
+                },
+                child: const Text('Go to Home')),
+          ),
+        ),
       ],
     );
   }
